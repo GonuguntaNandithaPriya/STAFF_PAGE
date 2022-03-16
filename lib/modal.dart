9e401a05e9b017c9b0330c2c412116355df6dd00
Nanditha Priya <gnpriya2000@gmail.com>
2022-03-16 16:54:38 +0530

    nandu

--- lib/modal.dart
+++ lib/modal.dart
@@ -30,3 +30,42 @@ class CardItem{
  
     
   ];
+
+
+
+  class CardItem1{
+  
+  final String title;
+  final String subtitle;
+  const CardItem1({required this.title,required this.subtitle});
+}
+List<CardItem1> items1 = [
+    const CardItem1(
+      title: 'AI LEVEL 1 Batch 5',
+      subtitle: 'Session-3',
+    ),
+    const CardItem1(
+      title: 'AI LEVEL 1 Batch 4',
+      subtitle: 'Session-10',
+    ),
+    const CardItem1(
+      title: 'AI LEVEL Batch 3',
+      subtitle: 'Session-12',
+    ),
+    const CardItem1(
+      title: '1',
+      subtitle: 'Today@1:30 PM',
+    ),
+    const CardItem1(
+      title: '2',
+      subtitle: 'Today@2:30 PM',
+    ),
+    const CardItem1(
+      title: '3',
+      subtitle: 'Today@1:30 PM',
+    ),
+    const CardItem1(
+      title: '4',
+      subtitle: 'Today@1:30 PM',
+    ),
+  ];
